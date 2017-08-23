10700e0899ebb87f2d4cfae816f5741445c938ba
Cody Miller <miskatonicraft@gmail.com>
2017-08-22 18:43:07 -0700

    Added initial splash page - Cody

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var allPaintings = [];
  * Global variable to store the index in allPaintings of the currently displayed painting
  * @Global {Number}
  */
-var currentPainting = -1;
+var currentPainting = 0;
 /**
  * Global Variable to store completed AJAX calls in chain
  * @Global {Number}
@@ -33,6 +33,9 @@ splashPage.artistName = "Endless Gallery";
 splashPage.paintingGallery = "LearningFuze, Irvine, CA";
 splashPage.galleryCoordinates.latitude = 33.6348748;
 splashPage.galleryCoordinates.longitude = -117.7404808;
+splashPage.paintingImage = "assets/images/code_screen_shot.png";
+splashPage.paintingMap = getMapElement(splashPage.galleryCoordinates.latitude, splashPage.galleryCoordinates.longitude);
+allPaintings.push(splashPage);
 
 /**
  * AJAX call to Artsy to receive random artwork with information
@@ -131,7 +134,7 @@ function getGalleryMap(response){
     allPaintings[allPaintings.length - 1].paintingMap = iframeElement;
     checkForAjaxCompletion();
 }
-function getInteractiveMapDOMElement(lat, long){
+function getMapElement(lat, long){
     var urlStr = "https://www.google.com/maps/embed/v1/view?key=AIzaSyDWPRK37JSNxBhmLhEbWzCQ57MQBQu8atk&center=" + lat + "," + long + "&zoom=18&maptype=satellite";
     var iframeElement = $('<iframe>',{
         frameborder: "0",
@@ -298,21 +301,13 @@ function Painting() {
 
 $(document).ready(function() {
     //$('.generateNewPainting').on('click', getNewPainting);
-    // var painting = new Painting();
-    // painting.paintingTitle = "Endless Gallery";
-    // painting.artistName = "Cody, Soren, and Grayson";
-    // painting.artistBiography = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, voluptatibus non ipsa. Perspiciatis numquam modi dolorum, est velit autem ipsum cupiditate! Assumenda earum quam iusto porro eum impedit recusandae et. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam, quis temporibus doloribus tenetur, iusto illum eius ipsum eaque aspernatur amet error iste assumenda minima ab deserunt unde fugiat! Similique, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio hic suscipit veniam officia autem, aliquam impedit magnam perferendis enim laudantium sunt, adipisci reprehenderit eos accusantium tempore non quo ipsum est.";
-    // painting.paintingImage = "/assets/code_screen_shot";
-    // painting.galleryCoordinates.latitude = 33.634692;
-    // painting.galleryCoordinates.longitude = -117.740296;
-    // painting.paintingMap
-
+    allPaintings[0].populatePage();
     getNewPainting();
-    // var timer = setInterval(function(){
-    //     if(allPaintings.length > 2) {
-    //         $("#nextPainting").on("click", nextPainting);
-    //         clearInterval(timer);
-    //     }
-    // },250);
-    // $("#previousPainting").on("click", previousPainting);
+    var timer = setInterval(function(){
+        if(allPaintings.length > 2) {
+            $("#nextPainting").on("click", nextPainting);
+            clearInterval(timer);
+        }
+    },250);
+    $("#previousPainting").on("click", previousPainting);
 });
